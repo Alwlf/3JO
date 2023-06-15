4680f4d7569fd5fe9976ea9e953935b15bf2d8a6
kimhwayoung <khyhannah0526@gmail.com>
2023-06-15 10:53:00 +0900

    a

--- projectapp/views.py
+++ projectapp/views.py
@@ -50,58 +50,58 @@ def disease(request):
 #                   {})
 
 
-### File Upload 처리하기
-def setFileInsert(request) :
-    try :
-        title = request.POST.get("title")
+# ### File Upload 처리하기
+# def setFileInsert(request) :
+#     try :
+#         title = request.POST.get("title")
 
-        if request.FILES.get("fileUpload") is not None :
-            file_nm = request.FILES.get("fileUpload")
-        else :
-            file_nm = ""
+#         if request.FILES.get("uploaded-image") is not None :
+#             file_nm = request.FILES.get("uploaded-image")
+#         else :
+#             file_nm = ""
 
-    except :
-        pass
-    if file_nm != "" :
-        ###########[ File Upload 처리하기 ]##########
-        ### - 파일 업로드 폴더 위치 지정 및 물리적 위치 생성하기
-        upload_dir = "./projectapp/static/projectapp/file_UpDown/"
-        download_dir = "./projectapp/static/projectapp/file_UpDown/"
+#     except :
+#         pass
+#     if file_nm != "" :
+#         ###########[ File Upload 처리하기 ]##########
+#         ### - 파일 업로드 폴더 위치 지정 및 물리적 위치 생성하기
+#         upload_dir = "./projectapp/static/projectapp/file_UpDown/"
+#         download_dir = "./projectapp/static/projectapp/file_UpDown/"
 
-        ### 파일(이미지)을 페이지에 보여줄 경우 : 폴더 전체 경로 지정
-        img_dir = "/static/projectapp/file_UpDown/"
+#         ### 파일(이미지)을 페이지에 보여줄 경우 : 폴더 전체 경로 지정
+#         img_dir = "/static/projectapp/file_UpDown/"
 
-        ### File_Uitl 클래스 생성하기
-        fu = File_Util()
+#         ### File_Uitl 클래스 생성하기
+#         fu = File_Util()
 
-        ### 초기값 셋팅(설정)하기
-        fu.setUpload(file_nm, upload_dir, img_dir, download_dir)
+#         ### 초기값 셋팅(설정)하기
+#         fu.setUpload(file_nm, upload_dir, img_dir, download_dir)
 
-        ### 파일 업로드 실제 수행하기*****
-        fu.fileUpload()
+#         ### 파일 업로드 실제 수행하기*****
+#         fu.fileUpload()
 
-        ########## [ 업로드된 파일 정보 조회 ] #########
-        ### 파일 사이즈
-        file_size = fu.file_size
-        ### 업로드된 파일명
-        filename = fu.filename
-        ### <img> 태그에 넣을 src 전체 경로
-        img_full_name = fu.img_full_name
-        ### (DB 저장용) 다운로드 전체경로+파일명
-        download_full_name = fu.download_full_name
+#         ########## [ 업로드된 파일 정보 조회 ] #########
+#         ### 파일 사이즈
+#         file_size = fu.file_size
+#         ### 업로드된 파일명
+#         filename = fu.filename
+#         ### <img> 태그에 넣을 src 전체 경로
+#         img_full_name = fu.img_full_name
+#         ### (DB 저장용) 다운로드 전체경로+파일명
+#         download_full_name = fu.download_full_name
 
-        ####### [Database 이용시]
-        # 컬럼은 두개사용 : img_full_name, download_full_name
+#         ####### [Database 이용시]
+#         # 컬럼은 두개사용 : img_full_name, download_full_name
         
-    # msg = """
-    #     <p><img src='{0}'></p>
-    # """.format(img_full_name, file_size, 
-    #            filename, download_full_name)
+#     # msg = """
+#     #     <p><img src='{0}'></p>
+#     # """.format(img_full_name, file_size, 
+#     #            filename, download_full_name)
     
-    return render(request,
-                  "projectapp/disease_result.html",
-                  {"img_full_name":img_full_name})
-    # return HttpResponse(msg)
+#     return render(request,
+#                   "projectapp/disease_result.html",
+#                   {"img_full_name":img_full_name})
+#     # return HttpResponse(msg)
 
 
 
@@ -193,7 +193,7 @@ def login_chk(request):
     msg = """
             <script type='text/javascript'>
                 alert('환영합니다. [{}]님 로그인 되었습니다.');
-                location.href = '/project/';
+                location.href = '/';
             </script>
     """.format(user_view.get("user_name"))
 
@@ -222,13 +222,15 @@ def setFileInsert(request) :
     try :
         title = request.POST.get("title")
 
-        if request.FILES.get("fileUpload") is not None :
-            file_nm = request.FILES.get("fileUpload")
+        if request.FILES.get("file-input") is not None :
+            file_nm = request.FILES.get("file-input")
         else :
             file_nm = ""
 
     except :
         pass
+    print(request.FILES)
+    print(request.POST)
     if file_nm != "" :
         ###########[ File Upload 처리하기 ]##########
         ### - 파일 업로드 폴더 위치 지정 및 물리적 위치 생성하기
@@ -365,12 +367,28 @@ def boardView(request):
     board_id = request.GET.get("board_id","ERROR")
 
 
-    board_vidw = Board.getBoardView(board_id)
+    board_view = Board.getBoardView(board_id)
 
 
     return render(request,		
                   "projectapp/board_view.html", 
-                  {"board_view":board_vidw})
+                  {"board_view":board_view})
+
+### 게시글 수정 폼
+def boardUpdateForm(request):
+
+    board_id = request.GET.get("board_id","ERROR")
+
+
+    board_view = Board.getBoardView(board_id)
+
+    return render(request,"projectapp/board_update_form.html",
+                  {"board_view":board_view})
+
+### 게시글 수정 
+def boardUpdate(request):
+
+    return HttpResponse("@")
 
 ### 게시글 작성
 def post(request):
