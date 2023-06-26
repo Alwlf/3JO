a33a0ea393e68b112ac9b980c7c0469deaa01357
umjunho <wns3147@naver.com>
2023-06-26 10:39:38 +0900

    230626

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u7w*b%#rh8zp)7yfn@3f2#*8*y+=2jx!1@r-xe072s4c!y%h4q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','192.168.56.101','localhost']
 
 
 # Application definition
--- projectapp/dc_model/model_pred.py
+++ projectapp/dc_model/model_pred.py
@@ -3,44 +3,6 @@ import numpy as np
 from PIL import Image
 import os
 
-<<<<<<< HEAD
-os.environ["CUDA_VISIBLE_DEVICES"]="-1"
-model_456=load_model("projectapp/dc_model/best_model_InceptionResNetV2_456.h5")
-model_136=load_model("projectapp/dc_model/best_model_InceptionResNetV2_136.h5")
-
-model_456.predict(np.arange(224*224*3).reshape(1,224,224,3))
-model_136.predict(np.arange(224*224*3).reshape(1,224,224,3))
-
-def pibu(img):
-    # 모델 클래스 
-    pibu_list_136=["구진 플라크","태선화 과다색소침착","결절종괴"]
-    pibu_list_456=["농포 여드름","미란 궤양","결절 종괴"]
-
-    pibu_list=["구진 플라크","","태선화 과다색소침착","농포 여드름","미란 궤양","결절종괴"]
-    # 이미지 전처리 
-    img=np.array(img.resize((224,224)))
-    img=img.reshape(1,224,224,3)/img.max()
-
-    # 예측
-    pred_136=model_136.predict(img)
-    pred_456=model_456.predict(img)
-    #print(pred_136,pred_456)
-    
-    # 2개 모델 결과 합치기
-    t=[0,0,0,0,0,0]
-    pred=np.hstack([pred_136[0],pred_456[0]])
-    for i,j in zip([0,2,5,3,4,5],pred):
-        print(i,j)
-        t[i]=j if t[i]<j else t[i]
-
-    # 확률과 병명
-    print(t)
-    idx=np.array(t).argmax()
-    p=np.round(t[idx],2)
-    b=pibu_list[idx]
-    return p,b
-    
-=======
 "cpu"
 os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 
@@ -97,4 +59,3 @@ def bot_model(dc,bot,img):
     p=np.round(pred[0][idx],2)
     b=classes[idx]
     return p,b
->>>>>>> 94e56aa79e9d3809588a7fe2c2e107b3942ceba7
